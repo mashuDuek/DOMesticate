c44a019d835703602fff85a5782b7c3432ad4489
Matthew Duek <mashuduek@gmail.com>
2018-04-02 15:01:26 -0400

    change entry point

--- webpack.config.js
+++ webpack.config.js
@@ -22,7 +22,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebPackPlugin({
-      template: "./src/index.html",
+      template: "./index.html",
       filename: "./index.html"
     })
   ]
